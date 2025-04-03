c33e237c14dd421bdad17f250530e6cd3f7ba7ae
elis1386 <elis1386@gmail.com>
2025-04-03 12:59:09 +0300

    add mediatR and automapper to project

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,8 +1,15 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseApiController : ControllerBase { }
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediotor;
+        protected IMediator Mediator =>
+            _mediotor ??= HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service is unavailable") ?? throw new InvalidOperationException("IMediator service is unavailable");
+    }
 }
